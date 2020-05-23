8ced221c260eddd2b3655bb2cb0262d1dce9ce10
rsitdikov <rsitdikov.usp@gmail.com>
2020-05-23 22:01:49 +0500

    5.3.3.1 Очередь на двух стеках[160#268894]

---
+++ chapter_001/src/main/java/collection/SimpleQueue.java
@@ -0,0 +1,30 @@
+package collection;
+
+import java.util.NoSuchElementException;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+    private int sizeIn = 0;
+    private int sizeOut = 0;
+
+    public T poll() throws NoSuchElementException {
+        if (sizeOut == 0) {
+            if (sizeIn == 0) {
+                new NoSuchElementException();
+            }
+            while (sizeIn > 0) {
+                out.push(in.pop());
+                sizeIn--;
+                sizeOut++;
+            }
+        }
+        sizeOut--;
+        return out.pop();
+    }
+
+    public void push(T value) {
+    in.push(value);
+    sizeIn++;
+    }
+}

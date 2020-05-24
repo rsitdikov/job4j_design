c4527e9f77ee8f28c2911eb9ad74272380c5415d
rsitdikov <rsitdikov.usp@gmail.com>
2020-05-24 13:13:31 +0500

    1. Создать модель User [#268911]

---
+++ chapter_001/src/main/java/collection/User.java
@@ -0,0 +1,27 @@
+package collection;
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+}

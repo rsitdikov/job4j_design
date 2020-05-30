da88da873830a344792afbbd7b5b31c3e72d9d25
rsitdikov <rsitdikov.usp@gmail.com>
2020-05-30 22:45:56 +0500

    4. Переопределить только equals[1004#268914]

--- chapter_001/src/main/java/collection/User.java
+++ chapter_001/src/main/java/collection/User.java
@@ -26,18 +26,23 @@ public class User {
         return birthday;
     }
 
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        User user = (User) o;
-//        return children == user.children &&
-//                name.equals(user.name) &&
-//                birthday.equals(user.birthday);
-//    }
-
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children
+                && name.equals(user.name)
+                && birthday.equals(user.birthday);
     }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, children, birthday);
+//    }
 }

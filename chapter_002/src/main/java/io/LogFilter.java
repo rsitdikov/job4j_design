a32489c0e0a23110dc0ee895fde6585d9c552564
rsitdikov <rsitdikov.usp@gmail.com>
2020-07-27 22:30:36 +0500

    0.4. BufferedOutputStream [#268932]

--- chapter_002/src/main/java/io/LogFilter.java
+++ chapter_002/src/main/java/io/LogFilter.java
@@ -1,7 +1,6 @@
 package io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,8 +22,21 @@ public class LogFilter {
         return rsl;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (String line : log) {
+                out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
-        System.out.println(log);
+        save(log, "404.txt");
     }
 }

e7b4a8bb8301ff90a02871eee185939444736097
rsitdikov <rsitdikov.usp@gmail.com>
2020-08-16 22:09:57 +0500

    3.0. Тестирование IO [#268928]

--- chapter_002/src/test/java/io/AnalizyTest.java
+++ chapter_002/src/test/java/io/AnalizyTest.java
@@ -1,16 +1,51 @@
 package io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 
 public class AnalizyTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void whenTempFolderIsUsed() throws IOException {
+        File source = folder.newFile("server.log");
+        File target = folder.newFile("unavailable.csv");
+        List<String> expected = List.of("10:57:01;10:59:01", "11:01:02;11:02:02");
+        List<String> result = new ArrayList<>();
+        try (PrintWriter out = new PrintWriter(source)) {
+            String log = String.join(System.lineSeparator(),
+                    "200 10:56:01",
+                    "500 10:57:01",
+                    "400 10:58:01",
+                    "200 10:59:01",
+                    "500 11:01:02",
+                    "200 11:02:02",
+                    ""
+            );
+            out.write(log);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            result = in.lines().collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertEquals(result, expected);
+    }
 
     @Test
     public void whenTwoPeriodsAreUnavailable() throws IOException {

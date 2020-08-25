f9bbea6e042ce1a5e80ae310ed0e3598900dbb76
rsitdikov <rsitdikov.usp@gmail.com>
2020-08-25 08:16:59 +0500

    4.1. Сканирование файловой системы.[106929#268927]

---
+++ chapter_002/src/main/java/io/Search.java
@@ -0,0 +1,19 @@
+package io;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.List;
+
+public class Search {
+
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        search(start, "js").forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}

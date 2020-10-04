506360562d7f46d18854e12afeda394d84905422
rsitdikov <rsitdikov.usp@gmail.com>
2020-10-04 22:12:52 +0500

    1. Бот[7921#268945]

--- chapter_002/src/main/java/io/EchoServer.java
+++ chapter_002/src/main/java/io/EchoServer.java
@@ -3,23 +3,31 @@ package io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
+        final Map<String, String> answers = Map.of(
+                "Exit", "Goodbye, dear friend.",
+                "Hello", "Hello, dear friend."
+        );
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str;
+                    String str, key;
                     while (!(str = in.readLine()).isEmpty()) {
-                        if (str.contains("Bye")) {
-                            server.close();
+                        if (str.contains("msg")) {
+                            key = str.split("msg=")[1].split(" ")[0];
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                            out.write(answers.getOrDefault(key, key).getBytes());
+                            if (key.equals("Exit")) {
+                                server.close();
+                            }
                         }
-                        System.out.println(str);
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
                 }
             }
         }

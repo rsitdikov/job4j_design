b0ce730272018a73097cdc2ffe3b6260101901ea
rsitdikov <rsitdikov.usp@gmail.com>
2021-09-13 21:46:01 +0500

    7. Scanner [#504791 #201119] - 2

---
+++ chapter_002/src/test/java/ru/job4j/io/scanner/CSVReaderTest.java
@@ -0,0 +1,137 @@
+package ru.job4j.io.scanner;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import ru.job4j.io.ArgsName;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class CSVReaderTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    @Test
+    public void whenFilterTwoColumns() throws Exception {
+        String data = String.join(
+                System.lineSeparator(),
+                "name;age;last_name;education",
+                "Tom;20;Smith;Bachelor",
+                "Jack;25;Johnson;Undergraduate",
+                "William;30;Brown;Secondary special"
+        );
+        File file = temporaryFolder.newFile("source.csv");
+        File target = temporaryFolder.newFile("target.csv");
+        ArgsName argsName = ArgsName.of(new String[]{
+                "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=" + target.getAbsolutePath(),
+                "-filter=name,age"
+        });
+        Files.writeString(file.toPath(), data);
+        String expected = String.join(
+                System.lineSeparator(),
+                "name;age",
+                "Tom;20",
+                "Jack;25",
+                "William;30"
+        ).concat(System.lineSeparator());
+        CSVReader.handle(argsName);
+        Assert.assertEquals(expected, Files.readString(target.toPath()));
+    }
+
+    @Test
+    public void whenFirstFilterWrong() throws IOException {
+        String data = String.join(
+                System.lineSeparator(),
+                "name;age;last_name;education",
+                "Tom;20;Smith;Bachelor",
+                "Jack;25;Johnson;Undergraduate",
+                "William;30;Brown;Secondary special"
+        );
+        File file = temporaryFolder.newFile("source.csv");
+        File target = temporaryFolder.newFile("target.csv");
+        ArgsName argsName = ArgsName.of(new String[]{
+                "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=" + target.getAbsolutePath(),
+                "-filter=surname,name"
+        });
+        Files.writeString(file.toPath(), data);
+        String expected = String.join(
+                System.lineSeparator(),
+                "name",
+                "Tom",
+                "Jack",
+                "William"
+        ).concat(System.lineSeparator());
+        CSVReader.handle(argsName);
+        Assert.assertEquals(expected, Files.readString(target.toPath()));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenAllFiltersWrong() throws IOException {
+        String data = String.join(
+                System.lineSeparator(),
+                "name;age;last_name;education",
+                "Tom;20;Smith;Bachelor",
+                "Jack;25;Johnson;Undergraduate",
+                "William;30;Brown;Secondary special"
+        );
+        File file = temporaryFolder.newFile("source.csv");
+        File target = temporaryFolder.newFile("target.csv");
+        ArgsName argsName = ArgsName.of(new String[]{
+                "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=" + target.getAbsolutePath(),
+                "-filter=surname,account"
+        });
+        Files.writeString(file.toPath(), data);
+        CSVReader.handle(argsName);
+    }
+
+    @Test
+    public void whenFilterOrderWrong() throws IOException {
+        String data = String.join(
+                System.lineSeparator(),
+                "name;age;last_name;education",
+                "Tom;20;Smith;Bachelor",
+                "Jack;25;Johnson;Undergraduate",
+                "William;30;Brown;Secondary special"
+        );
+        File file = temporaryFolder.newFile("source.csv");
+        File target = temporaryFolder.newFile("target.csv");
+        ArgsName argsName = ArgsName.of(new String[]{
+                "-path=" + file.getAbsolutePath(), "-delimiter=;", "-out=" + target.getAbsolutePath(),
+                "-filter=education,name"
+        });
+        Files.writeString(file.toPath(), data);
+        String expected = String.join(
+                System.lineSeparator(),
+                "name;education",
+                "Tom;Bachelor",
+                "Jack;Undergraduate",
+                "William;Secondary special")
+                .concat(System.lineSeparator());
+        CSVReader.handle(argsName);
+        Assert.assertEquals(expected, Files.readString(target.toPath()));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenParameterMissing() throws IOException {
+        String data = String.join(
+                System.lineSeparator(),
+                "name;age;last_name;education",
+                "Tom;20;Smith;Bachelor",
+                "Jack;25;Johnson;Undergraduate",
+                "William;30;Brown;Secondary special"
+        );
+        File file = temporaryFolder.newFile("source.csv");
+        File target = temporaryFolder.newFile("target.csv");
+        ArgsName argsName = ArgsName.of(new String[]{
+                "-path=" + file.getAbsolutePath(), "-out=" + target.getAbsolutePath(),
+                "-filter=surname,account"
+        });
+        Files.writeString(file.toPath(), data);
+        CSVReader.handle(argsName);
+    }
+}
